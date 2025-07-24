8ca52395be5e4ad81cdb2ae48e5ab07163dd132f
MASH2RAFI <mashrafiarefin.official@gmail.com>
2025-07-24 06:10:50 +0600

    login page

--- scrumspecter/src/app/(auth)/layout.tsx
+++ scrumspecter/src/app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 interface AuthLayoutProps {
     children: React.ReactNode;
@@ -7,7 +8,16 @@ const AuthLayout = ( { children } : AuthLayoutProps) => {
     return (
         <main className="bg-neutral-100 min-h-screen">
             <div className="mx-auto max-w-screen-2xl p-4">
-                {children }
+                <nav className="flex justify-between items-center">
+                    <Image src="/logo.svg" alt="logo" width={100} height={50} />
+                    <Button variant={"secondary"}>
+                        Sign Up
+                    </Button>
+                </nav>
+                <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+                    {children }
+                </div>
+                
             </div>
         </main>
     );
