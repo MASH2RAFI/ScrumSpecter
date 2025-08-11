357ef097520f46326351f618fa2434908c879293
MASH2RAFI <mashrafiarefin.official@gmail.com>
2025-08-11 06:21:15 +0600

    creating-member and generating invCode

--- scrumspecter/src/features/workspaces/server/route.ts
+++ scrumspecter/src/features/workspaces/server/route.ts
@@ -2,19 +2,39 @@ import { sessionMiddleware } from "@/lib/session-middleware";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWorkspaceSchema } from "../schemas";
-import { DATABASE_ID, WORKSPACES_ID } from "@/config";
-import { ID } from "node-appwrite";
+import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { ID, Query } from "node-appwrite";
+import { MemberRole } from "@/features/members/types";
+import { generateInviteCode } from "@/lib/utils";
 
 const app = new Hono()
     .get(
         "/", 
         sessionMiddleware,
         async (c) => {
-        const databases = c.get("databases");
-        const workspaces = await databases.listDocuments(
-            DATABASE_ID,
-            WORKSPACES_ID,
-        );
+            const user = c.get("user")
+            const databases = c.get("databases");
+
+            const members = await databases.listDocuments (
+                DATABASE_ID,
+                MEMBERS_ID,
+                [Query.equal("userId", user.$id)]
+            );
+            
+            if (members. total === 0) {
+                return c.json({ data: { documents: [], total: 0 } });
+            }
+            
+            const workspaceIds = members.documents.map((member) => member.workspaceId);
+
+            const workspaces = await databases.listDocuments(
+                DATABASE_ID,
+                WORKSPACES_ID,
+                [
+                    Query.orderDesc("$createdAt"),
+                    Query.contains("$id", workspaceIds)
+                ]
+            );
         return c.json({ data: workspaces });
     })
     .post(
@@ -32,8 +52,21 @@ const app = new Hono()
                 {
                     name,
                     userId: user.$id,
+                    inviteCode: generateInviteCode(6),
                 }
             );
+
+            await databases.createDocument(
+                DATABASE_ID,
+                MEMBERS_ID,
+                ID.unique(),
+                {
+                    userId: user.$id,
+                    workspaceId: workspace.$id,
+                    role: MemberRole.ADMIN,
+                },
+            )
+
             return c.json({data: workspace});
         }
 );
