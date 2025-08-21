8f41299ec1c6c155ecf46e7add7e98a9038d7ff7
MASH2RAFI <mashrafiarefin.official@gmail.com>
2025-08-22 01:35:53 +0600

    Building the data table

--- scrumspecter/src/features/tasks/server/route.ts
+++ scrumspecter/src/features/tasks/server/route.ts
@@ -6,7 +6,7 @@ import { getMember } from "@/features/members/utils";
 import { DATABASE_ID, MEMBERS_ID, PROJECTS_ID, TASKS_ID } from "@/config";
 import { ID, Query } from "node-appwrite";
 import { z } from "zod";
-import { TaskStatus } from "../types";
+import { Task, TaskStatus } from "../types";
 import { createAdminClient } from "@/lib/appwrite";
 import { Project } from "@/features/projects/types";
 
@@ -83,7 +83,7 @@ const app = new Hono()
                 query.push(Query.search("name", search));
             }
 
-            const tasks = await databases.listDocuments(
+            const tasks = await databases.listDocuments<Task>(
                 DATABASE_ID,
                 TASKS_ID,
                 query
@@ -109,7 +109,7 @@ const app = new Hono()
                 members.documents.map(async (member) => {
                     const user = await users.get(member.userId);
                     return {
-                        member,
+                        ...member,
                         name: user.name,
                         email: user.email,
                     }
